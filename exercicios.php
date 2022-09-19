aa6d4e71f32ce2ce1647208e0d95c3e67236efbb
andrea-s-m <andreamattevi02@gmail.com>
2022-09-19 14:44:00 -0300

    primos

--- exercicios.php
+++ exercicios.php
@@ -1,5 +1,20 @@
-<?php 
+<?php
+ 
+// numeros primos de 3 a 10, pois sabemos que o 2 ja é  
+ $primos = $i
+ array_push ($primos, $i);
 
-echo "ola mundo";
+for ($i = 3; $i <= 10; $i++) {
+    $divisores = 0;
      
+    for($a = $i; $a >= 1; $a--){
+    
+        if (($i % $a) == 0) {
+            $divisores++;
+        }
+    }
 
+    if ($divisores == 2){
+        echo os numeros primos sao "$primos";
+    }
+}
