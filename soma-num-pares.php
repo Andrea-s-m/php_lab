ac1a6735e24213ea12cea6c5b0d9cd9e446922c1
andrea-s-m <andreamattevi02@gmail.com>
2022-09-19 23:17:52 -0300

    soma dos numeors pares

---
+++ soma-num-pares.php
@@ -0,0 +1,13 @@
+<?php 
+
+$num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$pares = array();
+
+for($i=0; $i<= count($num); $i++) {
+    if ($i % 2 === 0) {
+    array_push($pares, $i);
+  
+}
+}
+
+echo "A soma dos pares é:" .array_sum($pares);
