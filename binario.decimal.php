e8a4e8f91454dd24b27badd3a44e2802ce6294e7
andrea-s-m <andreamattevi02@gmail.com>
2022-09-27 16:03:29 -0300

    binario-decimal

--- binario.decimal.php
+++ binario.decimal.php
@@ -8,6 +8,13 @@ $valorDecimal = 0;
 
 for ($i = 0; $i < count($arr); $i++){
     $valorCalculado = ($arr[$i] * pow(2,$valorPosicao));
+    // $valorCalculado = 1 * 2 no poder da $valorPosição(primeira vez : 6)
+    // $valorCalculado = 1 * 2 no poder da $valorPosição(segunda vez : 5)
+    // $valorCalculado = 1 * 2 no poder da $valorPosição(terceira vez : 4)
+    // $valorCalculado = 0 * 2 no poder da $valorPosição(quarta vez : 3)
+    // $valorCalculado = 0 * 2 no poder da $valorPosição(quinta vez : 2)
+    // $valorCalculado = 0 * 2 no poder da $valorPosição(sexta vez : 1)
+    // $valorCalculado = 1 * 2 no poder da $valorPosição(setima vez : 0)
 
     $valorDecimal += $valorCalculado;
     $valorPosicao--;
