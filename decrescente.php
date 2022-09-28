9c72b08e16fcb88721283d1705b439a92c6b6ec7
andrea-s-m <andreamattevi02@gmail.com>
2022-09-28 19:24:55 -0300

    binario-decimal

--- decrescente.php
+++ decrescente.php
@@ -1,7 +1,9 @@
 <?php
 
 $arr = array(13, 33, 3, 15, 02, 10, 02, 59, 60);
+
 $n1 = count($arr);
+
 for($i1 = 0; $i1 < $n1; $i1++){
 
     for ($j1 =0; $j1 < $n1 - $i1 - 1; $j1++)
@@ -14,6 +16,7 @@ for($i1 = 0; $i1 < $n1; $i1++){
         }
     }
 }
+
 foreach($arr as $vl){
     echo "{$vl} \n" ; 
 }
